74219e442c93f7ef86ed09edc93188a492c4f855
Ben B <31376764+lerminator3@users.noreply.github.com>
2020-06-11 12:28:20 -0400

    Added latest match and countdown clock features

--- success.php
+++ success.php
@@ -1,73 +1,72 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Your Information</title>
+        <title>Match Countdown</title>
         <link rel="stylesheet" href="dashstyles.css">
     </head>
     <body>
         <div id="phpHolder">
             <?php
-                //Set up arrays for every type of match (necessary for sorting matches by type)
                 $quals = array();
                 $eighths = array();
                 $quarters = array();
                 $semis = array();
                 $finals = array();
                 $none = array();
-                //This function will be to check the alliance of a given match, used to shorten its following function.
-                function checkAlliance($match_number, &$output_array, $team_key, $checked_array) {
+				$matches_sorted = array();
+                function addMatchInfo(&$output_array, $team_key, $checked_array, $match_type) {
                     if(in_array($team_key, $checked_array['alliances']['blue']['team_keys'])){
-                        //Add our match number and alliance color to the match type's array as an array
-                        $match_info = array($match_number, "blue");
+                        $match_info = array($checked_array['match_number'], "Blue", array_search($team_key, $checked_array['alliances']['blue']['team_keys'])+1, $checked_array['alliances']['blue']['team_keys'], $checked_array['predicted_time'], $checked_array['key'], $match_type);
                         array_push($output_array, $match_info);
                     }
                     elseif(in_array($team_key, $checked_array['alliances']['red']['team_keys'])){
-                        $match_info = array($match_number, "red");
+                        $match_info = array($checked_array['match_number'], "Red", array_search($team_key, $checked_array['alliances']['red']['team_keys'])+1, $checked_array['alliances']['red']['team_keys'], $checked_array['predicted_time'], $checked_array['key'], $match_type);
                         array_push($output_array, $match_info);
                     }
                 }
-                //This function can print our matches, their types, and their colors when given a $match_type string and the match's info.
                 function printMatch($match, $match_type) {
-                    if($match[1] == "blue") {
-                        echo "<p class='bluealliance'>" . $match_type . " Match " . $match[0] . "</p>";
+                    if($match[1] == "Blue") {
+                        echo "\n<p class='match bluealliance' onclick='openMatchData(" . '"' . $match[5] . '"' . ");'>" . $match_type . " Match " . $match[0] . "</p>";
                     }
-                    elseif($match[1] == "red") {
-                        echo "<p class='redalliance'>" . $match_type . " Match " . $match[0] . "</p>";
+                    elseif($match[1] == "Red") {
+                        echo "\n<p class='match redalliance' onclick='openMatchData(" . '"' . $match[5] . '"' . ");'>" . $match_type . " Match " . $match[0] . "</p>";
                     }
                 }
-                //This function is responsible for sorting our data, printing it in a human-readable format, and storing more info about a match. It needs an array that's been decoded from JSON for most of that, and a team key so it can see what alliance we're in
+                function printMatchInfo($match) {
+                    echo "\n<div id='". $match[5] ."' class='matchinfo'>";
+                    echo "<p>Match " . $match[0] . " info:</p>";
+                    echo "<p>Alliance Color &amp; Predicted Position: " . $match[1] . " " . $match[2] . "</p>";
+                    echo "<p>Alliance Members: "; print $match[3][0] ." ". $match[3][1] ." ". $match[3][2]; echo "</p>";
+                    echo "<p>Match Time: " . date("M d H:i:s", $match[4]) . "</p>";
+                    echo "</div>";
+                }
                 function getAllMatches($array, $team_key) {
                     global $quals, $eighths, $quarters, $semis, $finals, $none;
-                    //Now let's add match info to each array
                     foreach($array as $checked_array) {
                         $match_number = $checked_array['match_number'];
-                        //Check the match's type from the data TBA gave us
                         switch($checked_array['comp_level']) {
                             case "qm":
-                                //Check if our alliance is blue or red using the first function
-                                checkAlliance($match_number, $quals, $team_key, $checked_array);
+                                addMatchInfo($quals, $team_key, $checked_array, "Qualifiers");
                                 break;
                             case "ef":
-                                checkAlliance($match_number, $eighths, $team_key, $checked_array);
+                                addMatchInfo($eighths, $team_key, $checked_array, "Eighths Finals");
                                 break;
                             case "qf":
-                                checkAlliance($match_number, $quarters, $team_key, $checked_array);
+                                addMatchInfo($quarters, $team_key, $checked_array, "Quarterfinals");
                                 break;
                             case "sf":
-                                checkAlliance($match_number, $semis, $team_key, $checked_array);
+                                addMatchInfo($semis, $team_key, $checked_array, "Semifinals");
                                 break;
                             case "f":
-                                checkAlliance($match_number, $finals, $team_key, $checked_array);
+                                addMatchInfo($finals, $team_key, $checked_array, "Finals");
                                 break;
                             default:
-                                //If there's no match type, add the match number to its own category that we can deal with later
-                                checkAlliance($match_number, $none, $team_key, $checked_array);
+                                addMatchInfo($none, $team_key, $checked_array, "Not Specified");
                                 break;
                         }
                     }
-                    //Let's sort some stuff with an unnecessarily long function because PHP sucks (not as much as JS though, you can quote me).
                     $matches = array($quals, $eighths, $quarters, $semis, $finals, $none);
-                    $matches_sorted = array();
+                    global $matches_sorted;
                     function itemsort(&$array, $key) {
                         $sorter=array();
                         $ret=array();
@@ -85,61 +84,106 @@
                         itemsort($unsorted, "0");
                         array_push($matches_sorted, $unsorted);
                     }
-                    //Here, we can print out all of our values
-                    foreach($matches_sorted[0] as $match) {
-                        printMatch($match, "Qualifiers");
-                    }
-                    foreach($matches_sorted[1] as $match) {
-                        printMatch($match, "Eighths");
-                    }
-                    foreach($matches_sorted[2] as $match) {
-                        printMatch($match, "Quarterfinals");
-                    }
-                    foreach($matches_sorted[3] as $match) {
-                        printMatch($match, "Semis");
-                    }
-                    foreach($matches_sorted[5] as $match) {
-                        printMatch($match, "Finals");
-                    }
-                    foreach($matches_sorted[5] as $match) {
-                        printMatch($match, "No match type specified");
-                    }
+					foreach($matches_sorted as $matches) {
+						foreach($matches as $match) {
+                        	printMatch($match, end($match));
+                        	printMatchInfo($match);
+                    	}
+					}
                 }
-                //Here, stuff actually runs.
-                //If the team_key and event_key came from the input page, do stuff
-                if($_POST["team_key"] && $_POST["event_key"]) {
-                    //Assign our team key and event key to easily-usable variables
-                    $team_key = $_POST["team_key"];
+                function countDown() {
+					global $matches_sorted;
+                    foreach(array_reverse($matches_sorted) as $match_type){
+						if(!empty($match_type)){
+							$match_time = end($match_type)[4];
+							return(date("M d, Y H:i:s", $match_time));
+							break;
+						}
+					}
+                }
+                if(isset($_POST["team_key"]) && isset($_POST["event_key"])) {
+					if(strpos($_POST["team_key"], "frc") !== false) {
+						$team_key = $_POST["team_key"];
+					}
+					else {
+                    	$team_key = "frc" . $_POST["team_key"];
+					}
                     $event_key = $_POST["event_key"];
                     $api_key = "QszNNcJIpRcbbF8UIiU5WmqByHfIGaNFrTVcYR39DPlKft0Axtf31BXUL5rCmre4";
-                    //Separate the api key from the url so we can show it to the user (in the pits) without someone stealing my account
                     $request_url = "https://www.thebluealliance.com/api/v3/team/".$team_key."/event/".$event_key."/matches/simple?X-TBA-Auth-Key=";
                     $full_url = $request_url . $api_key;
-                    //Request the json from TBA
                     $response = file_get_contents($full_url);
-                    //Convert all of the JSON arrays into PHP arrays
                     $decoded_array = json_decode($response, true);
-                    
-                    //print out all of our data
                     echo "<p>Your Team Key: <span class=keys>$team_key</span></p>";
                     echo "<p>Your Event Key: <span class=keys>$event_key</span></p>";
-                    echo "<p>Request URL: $request_url (authkey hidden)</p>";
-                    //If the decoding was a success, do the following
+                    echo "<p>Request URL: " . $request_url . "(authkey hidden)</p>";
                     if($decoded_array) {
                         echo "<p>All matches in query:</p>";
-                        //Call our function from earlier giving it our decoded array and team key
+						echo "<div id='matches'>";
                         getAllMatches($decoded_array, $team_key);
+						echo "</div>";
+						echo "<div id='counterDiv'><h1 id='counter'></h1></div>";
                     }
-                    //Otherwise, tell the user there was no data.
                     else {
                         echo "<p>No data was returned from The Blue Alliance.</p>";
                     }
                 }
-                //Otherwise, go straight back to the input page
+				elseif(isset($_POST["team_key"]) && isset($_POST["latest"])) {
+					if(strpos($_POST["team_key"], "frc") !== false) {
+						$team_key = $_POST["team_key"];
+					}
+					else {
+                    	$team_key = "frc" . $_POST["team_key"];
+					}
+                    $api_key = "QszNNcJIpRcbbF8UIiU5WmqByHfIGaNFrTVcYR39DPlKft0Axtf31BXUL5rCmre4";
+					$event_request_url = "https://www.thebluealliance.com/api/v3/team/".$team_key."/events/simple?X-TBA-Auth-Key=".$api_key;
+					$event_response = file_get_contents($event_request_url);
+					$decoded_event = json_decode($event_response, true);
+					$event_key = end($decoded_event)["key"];
+                    $request_url = "https://www.thebluealliance.com/api/v3/team/".$team_key."/event/".$event_key."/matches/simple?X-TBA-Auth-Key=";
+                    $full_url = $request_url . $api_key;
+                    $response = file_get_contents($full_url);
+                    $decoded_array = json_decode($response, true);
+                    echo "<p>Your Team Key: <span class=keys>$team_key</span></p>";
+                    echo "<p>Your Event Key: <span class=keys>$event_key</span></p>";
+                    echo "<p>Request URL: " . $request_url . "(authkey hidden)</p>";
+                    if($decoded_array) {
+                        echo "<p>All matches in query:</p>";
+						echo "<div id='matches'>";
+                        getAllMatches($decoded_array, $team_key);
+						echo "</div>";
+						echo "<div id='counterDiv'><h1 id='counter'></h1></div>";
+                    }
+                    else {
+                        echo "<p>No data was returned from The Blue Alliance.</p>";
+                    }
+				}
                 else {
                     header("Location: index.html");
                 }
             ?>
         </div>
+		<script>
+			var matchTime = "<?php echo countDown();?>";
+			var countDownDate = new Date(matchTime).getTime();
+			var x = setInterval(function() {
+			  var now = new Date().getTime();
+			  var distance = countDownDate - now;
+			  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+			  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+			  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+			  document.getElementById("counter").innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
+			}, 1000);
+			function openMatchData(match) {
+				var x = document.getElementById(match);
+				if (x.style.display === "none") {
+					x.style.display = "block";
+				} 
+				else {
+					x.style.display = "none";
+				}
+			}
+		</script>
     </body>
 </html>
