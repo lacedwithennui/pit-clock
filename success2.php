0d6f0dfe851bb486401c1abf6685066a66269f56
Ben B <lerminator3@gmail.com>
2022-04-21 09:10:02 -0400

    fix timer ??

--- success2.php
+++ success2.php
@@ -297,6 +297,7 @@
                 }
                 virtualKettering();
                 echo "</div>";
+                echo "<p id='copy'>Copyright Hazel Belmont, FRC 5587 Titan Robotics</p>";
                 echo "<img src='logo.png' />";
             ?>
         </div>
@@ -309,7 +310,7 @@
 			var x = setInterval(function() {
 			  var now = convertTime(new Date()).getTime();
 			  var distance = countDownDate - now;
-			  var hours = Math.floor(((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))+1);
+			  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 			  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 			  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
